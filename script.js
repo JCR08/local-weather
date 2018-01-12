cc30bcae19cc623e2a3eade9d8a2e8acfeb7bcd8
John <j.c.rowell15@gmail.com>
2018-01-12 10:11:10 -0500

    worked on layout and added background images based on weather conditions

--- script.js
+++ script.js
@@ -20,8 +20,9 @@ function displayData(data){
   $("#degree").text(String.fromCharCode(176))
   $("#unit").text("C");
   $("#unit").click(tempUnit);
+  $("#weatherDesc").text(data.weather[0].main);
   $("#weatherIcon").attr("src", data.weather[0].icon);
-  console.log(data);
+  backgroundImage(data);
 }
 
 function tempUnit(){
@@ -45,5 +46,41 @@ function convertToCelsius(){
   var celsius = (fahrenheit -32) * (5/9);
   $("#temp").text(Math.ceil(celsius));
 }
+
+function backgroundImage(data){
+  var weather = data.weather[0].main;
+  switch(weather){
+    case 'Mist':
+      $('body').css({'background': "url('../images/mist.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Clouds':
+      $('body').css({'background': "url('../images/cloudy.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Rain':
+      $('body').css({'background': "url('../images/rain.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Snow':
+      $('body').css({'background': "url('../images/snow.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Thunderstorm':
+      $('body').css({'background': "url('../images/thunderstorm.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Drizzle':
+      $('body').css({'background': "url('../images/rain.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+    case 'Clear':
+      $('body').css({'background': "url('../images/clear.jpg') no-repeat",
+      "background-size" : "100%"})
+      break;
+  }
+  console.log(data);
+}
+
 
 getLocation();
