b5eb16d8ad16325c867d2febd6824008eeaf1357
John <j.c.rowell15@gmail.com>
2018-01-11 21:52:06 -0500

    displaying local weather

--- script.js
+++ script.js
@@ -10,8 +10,40 @@ function getLocation(){
 
 function getWeather(lat, lon){
   $.get(`https://fcc-weather-api.glitch.me/api/current?lon=${lon}&lat=${lat}`, function(data){
-    console.log(data);
+    displayData(data);
   })
+}
+
+function displayData(data){
+  $("#city").text(`${data.name}, ${data.sys.country}`);
+  $("#temp").text(Math.floor(data.main.temp))
+  $("#degree").text(String.fromCharCode(176))
+  $("#unit").text("C");
+  $("#unit").click(tempUnit);
+  $("#weatherIcon").attr("src", data.weather[0].icon);
+  console.log(data);
+}
+
+function tempUnit(){
+  if($("#unit").text() == "C"){
+    convertToFahrenheit();
+    $("#unit").text("F");
+  } else {
+    convertToCelsius();
+    $("#unit").text("C");
+  }
+}
+
+function convertToFahrenheit(){
+  var celsius = $("#temp").text();
+  var fahrenheit = celsius * (9/5) + 32;
+  $("#temp").text(Math.floor(fahrenheit))
+}
+
+function convertToCelsius(){
+  var fahrenheit = $("#temp").text();
+  var celsius = (fahrenheit -32) * (5/9);
+  $("#temp").text(Math.ceil(celsius));
 }
 
 getLocation();
